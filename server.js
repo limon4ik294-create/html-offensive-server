32fd0d50660015744886d6045ea61aa897a13f4c
limon4ik294-create <limon4ik294@gmail.com>
2026-03-05 13:36:30 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,29 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+// Раздаём статические файлы из папки frontend
+app.use(express.static(path.join(__dirname, "frontend")));
+
+// Главная страница
 app.get("/", (req, res) => {
-    res.send("HTML Offensive server is running 🔥");
+    res.sendFile(path.join(__dirname, "frontend", "index.html"));
 });
 
 io.on("connection", (socket) => {
     console.log("Player connected:", socket.id);
+
+    socket.on("disconnect", () => {
+        console.log("Player disconnected:", socket.id);
+    });
 });
 
 const PORT = process.env.PORT || 3000;
-
 server.listen(PORT, () => {
     console.log("Server started on port " + PORT);
 });
