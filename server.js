07c26f56a388c9285737c13e59a0c29fcf229940
limon4ik294-create <limon4ik294@gmail.com>
2026-03-05 13:43:51 +0200

    Update server.js

--- server.js
+++ server.js
@@ -52,8 +52,3 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log("Server started on port " + PORT);
 });
-
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log("Server started on port " + PORT);
-});
